1ec838fc2b1e4232aae4ea522738d50ca5083809
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-02-28 00:59:15 +0530

    Bug fixes of api data receive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,35 +4,48 @@ import useMenuDataFetch from "../util/useMenuDataFetch";
 import MenuCategories from "./MenuCategories";
 import { useState } from "react";
 
-const Menu = ()=>{
+const Menu = () => {
+    const { resId } = useParams();
+    const resInfo = useMenuDataFetch(resId);
 
-    const {resId} = useParams() 
-    const resInfo = useMenuDataFetch(resId)
+    const [showIndex, setShowIndex] = useState(null);
 
-    const[showIndex , setShowIndex] = useState(null);
+    if (resInfo == null) {
+        return <Shimmer />;
+    }
 
-if (resInfo == null) {return <Shimmer/>}
+    let name, cuisines, categories;
 
-
-const {name,cuisines} = resInfo?.cards[0]?.card?.card?.info
-
-const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((cat)=>{
-    return cat.card.card['@type'] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
- })
-
-console.log("Menu rerendered on expanding")
+    try {
+        name = resInfo?.cards[2]?.card?.card?.info?.name;
+        cuisines = resInfo?.cards[2]?.card?.card?.info?.cuisines;
+        categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(cat =>
+            cat.card.card['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        );
+    } catch (error) {
+        return (
+            <div className="text-center pt-8">
+                <h1>Error: {error.message}</h1>
+            </div>
+        );
+    }
 
     return (
         <div className="text-center pt-8">
-         <h1 className="font-bold text-3xl py-4">{name}</h1>
-         <h4 className="font-bold text-xl">{cuisines.join(", ")}</h4>
-         <div className="pt-10">
-            {categories.map((category , index)=>{
-              return  <MenuCategories key={category.card.card.title} data={category} itemView={index == showIndex ? true:false} setShowIndex = {()=>{setShowIndex(index)}}/>
-            })}
-         </div>
+            <h1 className="font-bold text-3xl py-4">{name}</h1>
+            <h4 className="font-bold text-xl">{cuisines.join(", ")}</h4>
+            <div className="pt-10">
+                {categories.map((category, index) => (
+                    <MenuCategories
+                        key={category.card.card.title}
+                        data={category}
+                        itemView={index === showIndex}
+                        setShowIndex={() => setShowIndex(index)}
+                    />
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Menu;
